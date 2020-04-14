4d8bebfb9f128e6d6e3fa71233361429d433fd58
Niema Moshiri <niemamoshiri@gmail.com>
2020-04-14 12:32:36 -0700

    Updated 2020-04-14

--- intermediate_files/treedater/run_treedater.R
+++ intermediate_files/treedater/run_treedater.R
@@ -3,5 +3,5 @@ tree <- ape::read.tree('/home/niemamoshiri/COVID19/intermediate_files/treedater/
 seqlen <- 29808
 times_tab <- read.csv('/home/niemamoshiri/COVID19/intermediate_files/treedater/times_treedater.txt', header=FALSE)
 times <- setNames(times_tab[,2], times_tab[,1])
-out <- dater(tree, times, seqlen, clock='uncorrelated', omega0=0.000004, numStartConditions=0, ncpu=4)
+out <- dater(tree, times, seqlen, clock='uncorrelated', omega0=0.000003, numStartConditions=0, ncpu=4)
 write.tree(out, '/home/niemamoshiri/COVID19/intermediate_files/treedater/dated.tre')
